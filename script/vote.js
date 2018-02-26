1106434a00d6f300a1e9be2af5351a0fc236f0a6
Jeremie Pereira <jeremie.pereira@schibsted.com>
2018-02-26 13:31:53 +0100

    Add templates and code basics

---
+++ script/vote.js
@@ -0,0 +1,44 @@
+// Utils
+
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+// Vote
+
+function getScore(team){
+
+    return (getCookie(team + '_score') !== null && +getCookie(team + '_score') > 0) ? +getCookie(team + '_score') : 0
+
+}
+
+function updateScore(team) {
+
+    document.getElementById('nb_vote').innerHTML = getScore(team)
+}
+
+function vote(team) {
+    let score = getScore(team)
+    setCookie(team + '_score', score + 1, 7)
+    console.log(score)
+    
+    updateScore(team)
+
+}
